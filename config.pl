bd89f5d93a1ab6f1e618e6a5a05e1fdcf7e2d37f
root <root@e-chome.com>
2010-10-02 11:22:23 +0900

    m

--- config.pl
+++ config.pl
@@ -14,36 +14,53 @@ return +{
     plugins => +{
         session => +{
             state => +{
-                class  => 'Kamui::Plugin::Session::State::Cookie',
+                class => 'Kamui::Plugin::Session::State::Cookie',
                 option => +{
-                    cookie_name    => 'twit_sid',
-                    cookie_domain  => $host, 
-                    cookie_path    => '/',
+                    cookie_name => 'twit_sid',
+                    cookie_domain => $host,
+                    cookie_path => '/',
                 },
             },
             store => +{
-                class  => 'Kamui::Plugin::Session::Store::Memcached',
+                class => 'Kamui::Plugin::Session::Store::Memcached',
                 option => +{
-                    servers    => [qw/localhost:11211/],
-                    expire_for => 60 * 60, 
+                    servers => [qw/localhost:11211/],
+                    expire_for => 60 * 60,
                 },
             },
         },
     },
     datasource => +{
-        dsn      => '',
+        dsn => '',
         username => '',
         password => '',
     },
     twitter_api => +{
-        api_key          => '',
-        callback_url     => '',
-        consumer_key     => '',
-        consumer_secret  => '',
-        request_token    => '',
+        api_key => '',
+        callback_url => '',
+        consumer_key => '',
+        consumer_secret => '',
+        request_token => '',
         access_token_url => '',
-        authorize_url    => '',
+        authorize_url => '',
+    },
+    validator_message => +{
+        param => +{
+            passwd  => 'パスワード',
+            name    => '名前',
+            text    => '本文',
+            email   => 'Email',
+            user_id => 'Twitter UserID',
+        },
+        function => +{
+            not_null    => '[_1]が空です',
+            'length'    => '[_1]の入力値がオーバーしています',
+            email_loose => '[_1]がEmailとして正しくありません',
+            ascii       => '[_1]に半角英数字以外が入力されています',
+            duplication => '[_1]が既に登録されています',
+        },
+        message => +{
+        },
     },
     timezone => 'Asia/Tokyo',
 };
-
